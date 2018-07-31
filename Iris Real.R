b80f0048622bebdbb46b8ffaa1fcafaa407e5e19
James-T54 <41272902+James-T54@users.noreply.github.com>
2018-07-31 00:14:49 -0400

    Add files via upload

--- Iris Real.R
+++ Iris Real.R
@@ -8,3 +8,18 @@ iris$Species<-gsub("%","",iris$Species)
 iris<-na.omit(iris)
 summary(iris)
 plot(iris)
+plot(iris, col=iris$Species)
+legend(7,4,3,unique(iris$Species),col=1 :length(iris$Species),pch=1)
+par(mfrow=c(1,2))
+plot(iris$Petal.Length)
+boxplot(iris$Petal.Length~ iris$Species) 
+par(mfrow=c(2,2)) 
+for(i in 1:4) boxplot(iris[,i]~ Species, data=iris, main=names(iris)[i])
+par(mfrow=c(1,1)) >hist(iris$Petal.Length[1:50]) 
+iris$Sepal.Length[1:50]
+iris$Sepal.Length[-(1:50)]
+iris$Sepal.Length[iris$Species == "setosa”]
+iris.ordered<-iris[order(iris$Sepal.Length),]
+
+
+
